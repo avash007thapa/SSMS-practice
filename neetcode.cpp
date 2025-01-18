0e2e2514b51602129ccc3a7f390ecd4332560d11
Avash <58122425+avash007thapa@users.noreply.github.com>
2025-01-17 16:58:58 -0800

    Hash practice
    Neetcode anagram question practice with strings and hashing

--- neetcode.cpp
+++ neetcode.cpp
@@ -1,4 +1,46 @@
-//NEETCODE 150- Contains Duplicates
+// NEETCODE 150- Valid Anagrams 
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // attempt at a hashing method
+
+        unordered_map<char, int> example;
+
+        // insert string s while also updating count value for repeat characters
+        for (int i = 0; i < s.length(); i++) {
+            char key = s[i];
+
+            // Search the key
+            auto it = example.find(key);
+
+            // Checking if key present or not
+            if (it != example.end())
+                example[key]++;
+            else
+                example.insert({key, 1});
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            auto it = example.find(t[i]);
+
+            if (it != example.end()){
+                example[t[i]]--;
+                if (example[t[i]] == 0)
+                    example.erase(t[i]);
+            }
+            else
+                return false;
+        }
+
+        if (example.empty())
+            return true;
+        else
+            return false;    
+    }
+};
+
+// NEETCODE 150- Contains Duplicates
 
 #include <vector>
 
